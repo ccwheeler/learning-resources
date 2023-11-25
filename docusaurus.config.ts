8fc38cd1de97a71c78f1c7af9306141f07bfdbcc
Charlotte Wheeler <charlotte.wheeler@hl.co.uk>
2023-11-25 16:45:25 +0000

    fix: :bug:

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 const config: Config = {
   title: "Learn with Lottie 🏴󠁧󠁢󠁷󠁬󠁳󠁿",
   tagline: "Dinosaurs are cool",
-  favicon: "img/favicon.ico",
+  // favicon: "img/favicon.ico",
 
   // Set the production url of your site here
   url: "https://your-docusaurus-site.example.com",
